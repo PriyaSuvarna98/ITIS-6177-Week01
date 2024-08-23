b6e6cd92d3bab46da552bf51c1c78406aa12ad48
PriyaSuvarna98 <priyasuvarna98@gmail.com>
2024-08-23 12:48:47 -0400

    Created a node server

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const {createServer} = require('node:http');
+
+const hostname = 'localhost';
+const port = 8084;
+
+const server = createServer((req, res) => {
+    res.statusCode =200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+})
+
+server.listen(port, hostname, ()=>{
+    console.log('Server is listening at port', port);
+})
+
